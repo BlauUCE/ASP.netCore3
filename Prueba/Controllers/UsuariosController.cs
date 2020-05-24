94cc4a5545270b88bd803fb43279638dc066fe9b
desarrolloASP <nb_correo@hotmail.com>
2020-05-24 15:25:35 -0500

    primer commit

---
+++ Prueba/Controllers/UsuariosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prueba.Controllers
+{
+    [Route("/Nombre")]
+    public class UsuariosController : Controller
+    {
+        //[HttpGet]
+        /*public IActionResult Index(String data, int number)
+        {
+            //ViewData["id"] = data+" "+number;
+            String datos = data + " " + number;
+            return View("Index", datos);
+        }*/
+
+        [Route("/Nombre/Alex")]
+        public IActionResult Index(String data, int number)
+        {
+            //ViewData["id"] = data+" "+number;
+            String datos = data + " " + number;
+            return View("Index", datos);
+        }
+    }
+}
