3b8e2bf23591391021fe76e7891e3582dcc9439e
desarrolloASP <nb_correo@hotmail.com>
2020-05-24 22:18:37 -0500

    hasta clase 11

--- Prueba/Controllers/UsuariosController.cs
+++ Prueba/Controllers/UsuariosController.cs
@@ -6,7 +6,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Prueba.Controllers
 {
-    [Route("/Nombre")]
+    
+    
+    //[Route("[controller]/[action]")]  //de ley pasar en la url controlador y action 
+    //[Route("/Nombre")]  //cambiar nombre de controlador
     public class UsuariosController : Controller
     {
         //[HttpGet]
@@ -17,12 +20,50 @@ namespace Prueba.Controllers
             return View("Index", datos);
         }*/
 
-        [Route("/Nombre/Alex")]
-        public IActionResult Index(String data, int number)
+        //[Route("/Nombre/uno")]
+        //[Route("/Nombre/otro/{data?}")]
+        //[Route("/[controller]/[action]/{data?}")]
+        //[HttpGet("/[controller]/[action]/{data?}")]
+        /*public IActionResult Index(String data, int number)
         {
             //ViewData["id"] = data+" "+number;
             String datos = data + " " + number;
             return View("Index", datos);
+        }*/
+
+        //[HttpGet("/[controller]/[action]/{data=int?}")]  //no funciona no puede ser opcional si hay tipo
+        //[HttpGet("/[controller]/[action]/{data=double}")]
+        //public IActionResult Index(double data)
+        //{
+        //    return View("Index", data);
+        //}
+
+        //[HttpGet("/[controller]/[action]/{data=int}")]
+        //public IActionResult Index(int data)
+        //{
+        //    return View("Index", data);
+        //}
+        //default
+        //public IActionResult Index()
+        //{ 
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            //var url = Url.Action("Metodo", "Usuarios", new { age=25, nombre="Juan" });
+            //return Content(url);
+
+            var url = Url.RouteUrl("nombre", new { age = 25, nombre = "Juan" });
+            return Redirect(url);
+        }
+
+
+        [HttpGet("/[controller]/[action]/{data=double}", Name = "nombre")] //asignar nombre a las rutas
+        public IActionResult Metodo(int age, string nombre)
+        {
+            var x = $"nombre {nombre} edad {age}";
+            return View("Index", x);
         }
     }
 }
